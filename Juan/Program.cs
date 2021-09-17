b3286b022fa17a5d04ecd7f0ecbe40c95105e9da
jumpert <88668277+jumpert@users.noreply.github.com>
2021-09-16 22:24:00 -0300

    Add files via upload

---
+++ Juan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Juan
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello");
+        }
+    }
+}
